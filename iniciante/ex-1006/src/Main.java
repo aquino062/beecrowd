edfce749831f4d98354b38448701f15fc9b777cd
willian aquino <willian_wjunior@hotmail.com>
2023-06-14 09:42:53 -0300

    1006

---
+++ iniciante/ex-1006/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+
+        nota1 *= 2;
+        nota2 *= 3;
+        nota3 *= 5;
+
+        double media = (nota1 + nota2 + nota3) / 10;
+
+        System.out.printf("MEDIA = %.1f\n", media);
+    }
+}
