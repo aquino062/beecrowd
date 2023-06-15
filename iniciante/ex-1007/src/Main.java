06abd9784ef04353dbdeabc8ddf87461b7670ebb
willian aquino <willian_wjunior@hotmail.com>
2023-06-15 14:22:31 -0300

    1007

---
+++ iniciante/ex-1007/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int result = (a * b) - (c * d);
+
+        System.out.println("DIFERENCA = " + result);
+        scanner.close();
+
+
+    }
+}
