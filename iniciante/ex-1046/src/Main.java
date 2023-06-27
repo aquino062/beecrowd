41cadd081b6de4a0ab96c6cb3a144d0f2d9988c5
willian aquino <willian_wjunior@hotmail.com>
2023-06-26 23:40:12 -0300

    1046

---
+++ iniciante/ex-1046/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inicio, fim, duracao;
+
+        inicio = scanner.nextInt();
+        fim = scanner.nextInt();
+
+
+        if (inicio >= fim) {
+            duracao = (24 + fim) - inicio;
+        } else {
+            duracao = fim - inicio;
+        }
+
+        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+    }
+}
