88d1e882f499f97bec82282c6cfca5012fc29b00
willian aquino <willian_wjunior@hotmail.com>
2023-07-07 19:47:43 -0300

    Update Main.java

--- iniciante/ex-1064/src/Main.java
+++ iniciante/ex-1064/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         int contador = 0;
         double somaTotal = 0, media;
 
