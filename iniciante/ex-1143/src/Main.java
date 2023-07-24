f6de070697d08c67405ab3ecc07a9352e3ceb8be
willian aquino <willian_wjunior@hotmail.com>
2023-07-24 13:11:01 -0300

    1143

---
+++ iniciante/ex-1143/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int resultado = 0;
+
+        int j = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int c = 1; c < 3; c++) {
+                resultado = j * j; //ao quadrado
+            }
+            System.out.print(j + " ");
+            System.out.print(resultado + " ");
+            System.out.println(resultado * j); // ao cubo
+            j += 1;
+        }
+        scanner.close();
+    }
+}
