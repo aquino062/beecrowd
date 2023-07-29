5279891e84e38a8976de66b19065b3ffec492138
willian aquino <willian_wjunior@hotmail.com>
2023-07-29 19:16:01 -0300

    Update Main.java

--- iniciante/ex-1154/src/Main.java
+++ iniciante/ex-1154/src/Main.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int soma = 0, contador = 0;
+
+        while (true) {
+            int idade = scanner.nextInt();
+
+            if (idade >= 0) {
+                soma += idade;
+                contador++;
+            } else {
+                break;
+            }
+        }
+        double media = ((double) soma / contador);
+
+        System.out.printf("%.2f\n",media);
+        scanner.close();
     }
-
-
 }
