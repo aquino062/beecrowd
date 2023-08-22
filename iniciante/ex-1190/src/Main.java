4de2fe37bcfb34ea40a1defe035d6062a28d1388
willian aquino <willian_wjunior@hotmail.com>
2023-08-22 16:49:52 -0300

    1190

---
+++ iniciante/ex-1190/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[][] m = new double[12][12];
+
+        char o = scanner.next().toUpperCase().charAt(0);
+        double soma = 0, media;
+        int cont = 0;
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                m[i][j] = scanner.nextDouble();
+
+            }
+        }
+
+
+        for (int i = 1; i <= 10; i++) {
+            if (i <= 5) {
+                for (int j = 11; j > 11 - i; j--) {
+                    soma += m[i][j];
+                    cont++;
+                }
+            } else {
+                for (int j = 11; j > i; j--) {
+                    soma += m[i][j];
+                    cont++;
+                }
+
+            }
+        }
+
+
+        media = soma / cont;
+
+        if (o == 'S') {
+            System.out.printf("%.1f\n", soma);
+        } else if (o == 'M') {
+            System.out.printf("%.1f\n", media);
+
+        }
+        scanner.close();
+
+    }
+}
