15fb58f6784eb5936218b08eaacc4afb1e528461
willian aquino <willian_wjunior@hotmail.com>
2023-09-02 08:53:49 -0300

    Update Main.java

--- iniciante/ex-1848/src/Main.java
+++ iniciante/ex-1848/src/Main.java
@@ -8,23 +8,31 @@ public class Main {
         int temp = 0;
 
         while (contador < 3) {
-            String a = scanner.nextLine();
+            String input = scanner.nextLine();
 
-            if (a.charAt(0) == '*') {
-                temp += 4;
-            }
-            if (a.charAt(1) == '*') {
-                temp += 2;
-            }
-            if (a.charAt(2) == '*') {
-                temp += 1;
-            }
+            temp += calcularValor(input);
 
-            if (a.equals("caw caw")) {
+            if (input.equals("caw caw")) {
                 System.out.println(temp);
                 temp = 0;
                 contador++;
             }
         }
+        scanner.close();
+    }
+
+    private static int calcularValor(String input) {
+        int valor = 0;
+        if (input.charAt(0) == '*') {
+            valor += 4;
+        }
+        if (input.charAt(1) == '*') {
+            valor += 2;
+        }
+        if (input.charAt(2) == '*') {
+            valor += 1;
+        }
+
+        return valor;
     }
 }
