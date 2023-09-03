80f40ac628810048362c6cd5afd19dafdaf5a30c
willian aquino <willian_wjunior@hotmail.com>
2023-09-03 11:22:00 -0300

    ex-1924

---
+++ iniciante/ex-1924/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+            String curso = scanner.nextLine();
+        }
+        System.out.println("Ciencia da Computacao");
+        scanner.close();
+
+    }
+}
