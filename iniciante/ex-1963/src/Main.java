928fae4d003745dddae2914df8ab47ddcbe6e791
willian aquino <willian_wjunior@hotmail.com>
2023-09-08 15:00:58 -0300

    ex-1963

---
+++ iniciante/ex-1963/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double novoPreco = (b - a) / a * 100;
+
+        System.out.printf("%.2f%%\n", novoPreco);
+        scanner.close();
+    }
+}
